49983cdc27a5223bc6c46b4119ee31236ecd4a6b
marlon <mcastano@robinfood.com>
2022-03-11 21:40:07 -0500

    DTO aun no funcional de cines

--- src/Model/DTO_boletas.js
+++ src/Model/DTO_boletas.js
@@ -3,6 +3,28 @@ const bd = require('../bd_connection')
 const getboletas = async() => {
     const res = await bd.query('select * from boletas;')
     console.log(res)
+    return res
+};
+
+const sendboletas = async(boleta) => {
+    const res = await bd.query(`insert into boletas (precios,categoria,descuento) values (${boleta.precios}, '${boleta.categoria}', ${boleta.descuento});`)
+    console.log(res)
+    return res
+};
+
+const updatedboletas = async(boleta) => {
+    const res = await bd.query(`update boletas set precios = ${boleta.precios}, categoria = '${boleta.categoria}', descuento = ${boleta.descuento} where id=${boleta.id};`)
+    console.log(res)
+    return res
+};
+
+const deleteboletas = async(boleta) => {
+    const res = await bd.query(`delete from boletas where id=${boleta.id};`)
+    console.log(res)
+    return res
 };
 
 module.exports = getboletas
+module.exports = sendboletas
+module.exports = updatedboletas
+module.exports = deleteboletas
--- src/routes/boletas.js
+++ src/routes/boletas.js
@@ -1,13 +1,45 @@
 const express = require('express')
+const { sendfile } = require('express/lib/response')
+const deleteboletas = require('../Model/DTO_boletas')
+const updatedboletas = require('../Model/DTO_boletas')
+const sendboletas = require('../Model/DTO_boletas')
 const getboletas = require('../Model/DTO_boletas')
 
 const router = express.Router()
 
 router.get(
     '/getboletas',
-    (req, res) => {
+    async(req, res) => {
         getboletas()
-        console.log('hola')
+        res.json(await getboletas())
     }
 )
+
+router.post(
+    '/sendboletas',
+    async(req, res) => {
+        await sendboletas(req.body)
+        res.statusCode = 200
+        return res.json({ status: "buen trabajo " })
+    }
+)
+
+router.put(
+    '/updateboletas',
+    async(req, res) => {
+        await updatedboletas(req.body)
+        res.statusCode = 200
+        return res.json({ status: "buen trabajo " })
+    }
+)
+router.delete(
+    '/deleteboletas',
+    async(req, res) => {
+        await deleteboletas(req.body)
+        res.statusCode = 200
+        return res.json({ status: "buen trabajo " })
+    }
+)
+
+
 module.exports = router
